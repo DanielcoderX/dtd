86efebef3f154588d73171da06bc6d8967646c01
DanielcoderX <danyalghlaby@gmail.com>
2023-06-11 21:12:04 +0330

    Add new feature: Calculate date differences in days, months, and years

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # dtd
 
-This module contains a method to calculate the difference between two dates in days, months, and years.
+This module provides a convenient way to calculate the difference between two dates in days, months, and years. It offers a simple and effective solution for performing date calculations in Crystal applications.
 
 ## Installation
 
-1. Add the dependency to your `shard.yml`:
+1. Add the `dtd` dependency to your `shard.yml` file:
 
    ```yaml
    dependencies:
@@ -12,31 +12,45 @@ This module contains a method to calculate the difference between two dates in d
        github: danielcoderx/dtd
    ```
 
-2. Run `shards install`
+2. Run `shards install` to fetch the dependency.
 
 ## Usage
 
 ```crystal
 require "dtd"
+
 date1 = "2022/04/30"
 date2 = "2022/05/01"
+
 result = Dtd.date_diff(date1, date2)
 
-puts "Difference: \#{result["years"]} years, \#{result["months"]} months, \#{result["days"]} days"
+puts "Difference: #{result["years"]} years, #{result["months"]} months, #{result["days"]} days"
 ```
 
+## Features
+
+- Calculates the difference between two dates in days, months, and years.
+- Supports custom time zones for accurate calculations.
+- Returns a Hash that provides the difference in years, months, and days.
+- Handles scenarios where the second date is before the first date.
+- Provides an error message when necessary.
+
 ## TODO
-- Add time difference 
-- You can say :) just open an issue
+
+- Enhance error handling and error messages.
+- Add support for accepting various input formats.
+- Extend functionality to calculate time differences.
 
 ## Contributing
 
-1. Fork it (<https://github.com/danielcoderx/dtd/fork>)
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Add some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create a new Pull Request
+1. Fork the repository from [https://github.com/danielcoderx/dtd](https://github.com/danielcoderx/dtd).
+2. Create your feature branch: `git checkout -b my-new-feature`.
+3. Commit your changes: `git commit -am 'Add some feature'`.
+4. Push the branch to your forked repository: `git push origin my-new-feature`.
+5. Create a new Pull Request.
 
 ## Contributors
 
-- [danielcoderx](https://github.com/danielcoderx) - creator and maintainer
+- [danielcoderx](https://github.com/danielcoderx) - Creator and maintainer
+
+Your contributions to this project are highly appreciated! If you have any suggestions, bug reports, or feature requests, please don't hesitate to open an issue or submit a pull request. Let's work together to make `dtd` even better!
--- src/dtd.cr
+++ src/dtd.cr
@@ -3,24 +3,21 @@ module Dtd
   # The current version of the Dtd module.
   VERSION = "0.1.0"
 
-  # <h3>Calculates the difference between two dates in days, months, and years.</h3>
+  # Calculates the difference between two dates in days, months, and years.
   #
-  # &nbsp;&nbsp;&nbsp;&nbsp;date1     - The first date in the format YYYY/MM/DD (String).
-  # 
-  # &nbsp;&nbsp;&nbsp;&nbsp;date2     - The second date in the format YYYY/MM/DD (String).
-  # 
-  # &nbsp;&nbsp;&nbsp;&nbsp;time_zone - The time zone in which the dates are specified (String, default: "UTC").
+  # date1     - The first date in the format YYYY/MM/DD (String).
+  # date2     - The second date in the format YYYY/MM/DD (String).
+  # time_zone - The time zone in which the dates are specified (String, default: "UTC").
   #
-  # <b>Examples:</b>
+  # Examples:
+  #   Dtd.date_diff("2022/04/30", "2022/05/01")
+  #   => {"years" => 0, "months" => 0, "days" => 1}
   #
-  #   &nbsp;&nbsp;&nbsp;Dtd.date_diff("2022/04/30", "2022/05/01")
-  #   &nbsp;&nbsp;=> {"years" => 0, "months" => 0, "days" => 1}
-  #
-  # Returns a Hash containing the difference in years, months, and days, or an error Hash.</br>
-  # &nbsp;If the difference is positive, the Hash contains the keys "years", "months", and "days"
-  # with their respective integer values.</br> &nbsp;If date2 is before date1, the Hash contains the key
-  # "error" with the value "Date 2 is before date 1.".</br> &nbsp;If the difference is 0, the Hash
-  # contains the keys "years", "months", and "days" with the value 0 for each.
+  # Returns a Hash containing the difference in years, months, and days, or an error Hash.
+  # If the difference is positive, the Hash contains the keys "years", "months", and "days"
+  # with their respective integer values.
+  # If date2 is before date1, the Hash contains the key "error" with the value "Date 2 is before date 1."
+  # If the difference is 0, the Hash contains the keys "years", "months", and "days" with the value 0 for each.
   def self.date_diff(date1 : String, date2 : String, time_zone : String = "UTC") : Hash(String, Int32) | Hash(String, String)
     # Load the specified time zone.
     time_zone = Time::Location.load(time_zone)
@@ -47,6 +44,43 @@ module Dtd
       
       # Create a Hash containing the difference in years, months, and days.
       {"years" => years, "months" => months, "days" => days}
+    end
+  end
+
+  # Calculates the difference between two times in hours, minutes, and seconds.
+  #
+  # time1     - The first time in the format YYYY/MM/DD HH:MM:SS (String).
+  # time2     - The second time in the format YYYY/MM/DD HH:MM:SS (String).
+  # time_zone - The time zone in which the times are specified (String, default: "UTC").
+  #
+  # Examples:
+  #   Dtd.time_diff("2022/01/01 08:45:00", "2022/01/01 11:30:15")
+  #   => {"hours" => 2, "minutes" => 45, "seconds" => 15}
+  #
+  # Returns a Hash containing the difference in hours, minutes, and seconds, or an error Hash.
+  # If the difference is positive, the Hash contains the keys "hours", "minutes", and "seconds"
+  # with their respective integer values.
+  # If time2 is before time1, the Hash contains the key "error" with the value "Time 2 is before time 1."
+  # If the difference is 0, the Hash contains the keys "hours", "minutes", and "seconds" with the value 0 for each.
+  def self.time_diff(time1 : String, time2 : String, time_zone : String = "UTC") : (Hash(String, Int32 | Int64) | Hash(String, Int32) | Hash(String, String))
+    time_zone = Time::Location.load(time_zone)
+    
+    time1_time = Time.parse(time1, "%Y/%m/%d %H:%M:%S", time_zone)
+    time2_time = Time.parse(time2, "%Y/%m/%d %H:%M:%S", time_zone)
+    
+    seconds_diff = (time2_time - time1_time).to_i
+    
+    if seconds_diff.zero?
+      {"hours" => 0, "minutes" => 0, "seconds" => 0}
+    elsif seconds_diff < 0
+      {"error" => "Time 2 is before time 1."}
+    else
+      hours = (seconds_diff / 3600).to_i
+      seconds_diff %= 3600
+      minutes = (seconds_diff / 60).to_i
+      seconds = seconds_diff % 60
+      
+      {"hours" => hours, "minutes" => minutes, "seconds" => seconds}
     end
   end
 end 
